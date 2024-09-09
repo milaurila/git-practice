ed93ccdf1fd46d502302265679dd0eef1318ff62
milaurila <michael.laurila@gmail.com>
2024-09-09 14:36:06 +0200

    Add subtraction method

--- calculator.py
+++ calculator.py
@@ -7,3 +7,9 @@ class Calculator:
 
     def float_addition(float a, float b):
         return a + b
+
+    def int_subtraction(int a, int b):
+        return a - b
+
+    def float_subtraction(float a, float b):
+        return a - b
